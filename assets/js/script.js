5ae8c5ee97bfc5aab5380e8f919eaa03597df644
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-01-21 10:28:39 -0600

    pseudocoding in js file

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// display date at top of calendar
+
+// time blocks display for standard buisness hours 9-5
+
+// time blocks are color coded based on past, present, or future
+
+// when a time block is clicked, you can enter text
+
+// when the save button is clicked, it is saved in local storage
+
+// when page is refreshed, the save events still exist
