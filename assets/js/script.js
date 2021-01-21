5ee23d40050a60edfcd7ecd675fb39885e0a5ba4
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-01-21 10:58:48 -0600

    hour of the day blocks are displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 // display date at top of calendar
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
 // time blocks display for standard buisness hours 9-5
+var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+
+for (var i = 0; i < 9; i++){
+    var hoursOfDay = $("<div>");
+    $(".container").append(hoursOfDay);
+    hoursOfDay.attr("class", "time-block row description");
+    hoursOfDay.text(hours[i]);
+}
 
 // time blocks are color coded based on past, present, or future
 
