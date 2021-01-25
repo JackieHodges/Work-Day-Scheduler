3beff79ed62e2e7d7a4c520a91a1d4c6cdfc9db5
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-01-25 11:44:14 -0600

    attempt to change class of timeblock

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,21 +53,19 @@ $(".hour").each(function(){
     if (isSame === true && isAfter === false){
         console.log("this is the current event");
         // add class of pressent
+        $('.description').attr("class", "col-8 description present");
     } else if (isSame == false && isAfter === true){
         console.log("this is a future event");
         // add class of future
+        $('.description').attr("class", "col-8 description future");
     } else {
         console.log("this is a past event");
         // add class of past
+        $('.description').attr("class", "col-8 description past");
     }
 
 });
 
-
-
-// when a time block is clicked, you can enter text
-
 // when the save button is clicked, it is saved in local storage
-
 
 // when page is refreshed, the save events still exist
